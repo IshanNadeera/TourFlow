36b6a572ccf3f1cd967606cab296d1c52dbf74a6
Keshara Bandara <dimaginipunika@gmail.com>
2023-03-22 14:15:18 +0530

    Implementing  Restaurants initial Page

--- TourFlow/src/screens/RestaurantManagement/RestaurantView.js
+++ TourFlow/src/screens/RestaurantManagement/RestaurantView.js
@@ -1,12 +1,41 @@
 import React from "react";
-import {View, Text, StyleSheet, SafeAreaView} from 'react-native'
+import {View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity} from 'react-native'
 import Colors from "../../utils/Colors";
+import Lottie from 'lottie-react-native';
 
 
 const Restaurant = ({navigation}) => {
+
+    const selectGuide = () => {
+        navigation.navigate('Initial');
+    };
+
     return(
         <SafeAreaView style={styles.container}>
-            <Text>Restaurant</Text>
+            <View style={{flex:2.5, marginTop: 10}}>
+                <View style={{flexDirection: 'row', alignSelf: 'center'}}>
+                    <Text style = {{fontSize:50, fontWeight: 'bold', color: Colors.resturantColor, marginTop: 10, marginLeft: 10}}>Resturants</Text>
+                </View>
+                <Lottie style={{width: '80%',marginLeft:20, marginTop:-20}} source={require('../../../img/cooking.json')} autoPlay loop />
+                <Text style = {{fontSize:22, fontWeight: 'bold', color: Colors.fontColor4, textAlign: 'center'}}>Discover the Best Sri Lankan Taste</Text>
+            </View>
+
+            <View style={styles.BottomView}>
+                
+            <Text style = {{fontSize:15, fontWeight: 'bold', color: Colors.fontColor1, textAlign: 'center',width:330,marginTop:-90}}> Based on the compilation of hundreds of guidebooks, thousands of media publications, and millions of online reviews, Tour Flow offers the world’s best restaurant selection at your fingertips wherever you go.</Text>
+          
+                     <View style={styles.buttonContent}>
+                        <TouchableOpacity style={styles.button} onPress={selectGuide}>
+                            <Text style={{color: '#fff', fontSize: 16}}>Explore More</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <TextInput
+                        placeholder='Search By Your Location'
+                        style={styles.input}
+                    />
+            </View>
+           
+
         </SafeAreaView>
     )
 }
@@ -17,5 +46,57 @@ const styles = StyleSheet.create({
     container: {
         flex : 1,
         backgroundColor : Colors.bgColor
-    }
+    },
+
+    BottomView: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    formContainer: {
+        backgroundColor: Colors.fontColor2,
+        borderRadius: 10,
+        marginVertical: 100,
+        marginHorizontal: 0,
+        elevation: 3,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    textLabel : {
+        alignSelf : 'flex-start',
+        color: Colors.mainColor2,
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginTop: 15,
+        marginLeft: 10,
+    },
+
+    input: {
+        height: 50,
+        marginTop: -15,
+        borderWidth: 1,
+        borderColor : 'grey',
+        padding: 10,
+        color: 'black',
+        width: '95%',
+        borderRadius: 10,
+        backgroundColor: 'white'
+    },
+    buttonContent: {
+        alignSelf : 'center',
+        marginTop: 20,
+        marginBottom: 30
+    },
+
+    button: {
+        backgroundColor: Colors.mainColor2,
+        borderRadius: 10,
+        width: 150,
+        height: 50,
+        alignItems : 'center',
+        justifyContent : 'center'
+    },
+
 });
--- TourFlow/src/utils/Colors.js
+++ TourFlow/src/utils/Colors.js
@@ -3,5 +3,7 @@ export default{
     mainColor2 : '#00b35d',
     mainColor3 : '#ff0000',
     fontColor1 : '#000',
-    fontColor2 : '#fff'
+    fontColor2 : '#fff',
+    fontColor4 : '#5ca904',
+    resturantColor : '#087830'
 }
