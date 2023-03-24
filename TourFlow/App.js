14b5602939bb80044164a130ab46e5f5ddff5394
Ishan Nadeera <IT20016616@my.sliit.lk>
2023-03-24 12:18:44 +0530

    Created location add view

--- TourFlow/App.js
+++ TourFlow/App.js
@@ -6,6 +6,7 @@ import Tabs from './src/navigation/Tabs';
 import LocationSingle from './src/screens/LocationManagement/LocationSingleView';
 import GuideInformation from './src/screens/GuideManagement/GuideInformation';
 import Register from './src/screens/Register';
+import LocationAdd from './src/screens/LocationManagement/LocationAddView';
 
 const Stack = createNativeStackNavigator();
 
@@ -19,6 +20,8 @@ const App = () => {
         <Stack.Screen name='Initial' component={Tabs} options={{ headerShown : false}} />
 
         <Stack.Screen name='LocationSingle' component={LocationSingle} options={{ headerShown : false}} />
+        <Stack.Screen name='LocationAdd' component={LocationAdd} options={{ headerShown : false}} />
+        
         <Stack.Screen name='GuideSingle' component={GuideInformation} options={{ headerShown : false}} />
       </Stack.Navigator>
     </NavigationContainer>
